94a7f4af8b12940720f46c4a5867b6774933fd81
Gabriel Weinstock <weinstock.gabriel@gmail.com>
2020-09-17 20:04:03 -0400

    adding server files

---
+++ server.js
@@ -0,0 +1,26 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  server.send([Buffer.from('repl: ' + msg)],rinfo.port, rinfo.address, (err) => {
+      console.log(`err: ${err}`);
+  });
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.on('close', () => {
+  console.log('socket close');
+});
+
+server.bind(41234);
+// server listening 0.0.0.0:41234
