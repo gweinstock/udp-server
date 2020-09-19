96e850973853ba7cfe8240e50b1d9c67c423d66c
Gabriel Weinstock <weinstock.gabriel@gmail.com>
2020-09-19 16:25:26 -0400

    adding ioredis

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+var fs = require('fs');
+var ioredis = require('ioredis');
+
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+
+// load redis:
+var redis;
+
+fs.readFile('redis-cfg.json', 'utf8', function(err, data) {
+  var servers = JSON.parse(data.toString()).cache_servers;
+  redis = new ioredis.Cluster(servers, { redisOptions: { password: '153aaa4ff88b9e54925777480b14652ed9822baf4363b1f35114f7c0d09a54c7' } });
+});
+
+var idSeq = 1;
 
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
@@ -7,16 +20,28 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-  console.log(msg.length);
   var s = Buffer.from(msg.toString('hex').replace(/cc/g, '').replace(/00/, ''), 'hex').toString();
   console.log(`server got: ${s} from ${rinfo.address}:${rinfo.port}`);
   var obj = JSON.parse(s);
   if (obj.msgType == 'connect') {
-    console.log('got "connect"');
+    console.log('connect');
+    redis.get('udp-clients', function(err, clients) {
+      var cli;
+      if (clients) {
+        cli = JSON.parse(clients);
+        cli.push({id:idSeq,host:rinfo.address,port:rinfo.port});
+      } else {
+        cli = [{id:idSeq,host:rinfo.address,port:rinfo.port}];
+      }
+      idSeq++;
+      redis.set('udp-clients', JSON.stringify(cli));
+    });
+    server.send([Buffer.from('{"msgType":"connect","id":' + idSeq + '}')],rinfo.port, rinfo.address, (err) => {
+      console.log(`sent connect to: ${rinfo.address}:${rinfo.port}`);
+    });
+  } else {
+    console.log('unknown command');
   }
-  server.send([Buffer.from('{"msgType":"connect","seq":1}')],rinfo.port, rinfo.address, (err) => {
-      console.log(`to: ${rinfo.address}:${rinfo.port}`);
-  });
 });
 
 server.on('listening', () => {
